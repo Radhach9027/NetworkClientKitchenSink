515db1d0b3bb1f3a12017212da81a7bf292768c3
ChandanC <chandan@viba.ai>
2022-11-03 22:38:54 +0530

    Refactor network extension

--- NetworkClientSink/Extensions/Network+Extensions.swift
+++ NetworkClientSink/Extensions/Network+Extensions.swift
@@ -2,43 +2,37 @@ import Foundation
 import NetworkClient
 
 extension Network {
-    
-    class var certificate: SecCertificate? {
-        switch SecCertificate.loadFromBundle() {
-            case .success(let cert):
-                return cert
-            case .failure(let error):
-                print(error)
-                return nil
-        }
-    }
-    
     class var defaultSession: Network {
-        guard let certificate = certificate else {
+        switch SecCertificate.loadFromBundle() {
+        case let .success(certificate):
+            return Network(
+                config: .default(),
+                pinning: SSLPinning.certificatePinning(
+                    certificate: certificate,
+                    hash: SecCertificate.hashKey
+                )
+            )
+        case .failure:
             return Network(config: .default())
         }
-        
-        let network = Network(
-            config: .default(),
-            pinning: SSLPinning.certificatePinning(certificate: certificate, hash: SecCertificate.hashKey)
-        )
-        return network
-        
     }
-    
-    class func backgroundSession(urlSessionDidFinishEvents: @escaping (URLSession)-> Void) -> Network {
-        guard let certificate = certificate else {
+
+    class func backgroundSession(urlSessionDidFinishEvents: @escaping (URLSession) -> Void) -> Network {
+        switch SecCertificate.loadFromBundle() {
+        case let .success(certificate):
+            return Network(
+                config: .background(identifer: Bundle.identifier),
+                pinning: SSLPinning.certificatePinning(
+                    certificate: certificate,
+                    hash: SecCertificate.hashKey
+                ),
+                urlSessionDidFinishEvents: urlSessionDidFinishEvents
+            )
+        case .failure:
             return Network(
                 config: .background(identifer: Bundle.identifier),
                 urlSessionDidFinishEvents: urlSessionDidFinishEvents
             )
         }
-        
-        let network = Network(
-            config: .background(identifer: Bundle.identifier),
-            pinning: SSLPinning.certificatePinning(certificate: certificate, hash: SecCertificate.hashKey),
-            urlSessionDidFinishEvents: urlSessionDidFinishEvents
-        )
-        return network
     }
 }
--- NetworkClientSink/Extensions/SecCertificate+Extensions.swift
+++ NetworkClientSink/Extensions/SecCertificate+Extensions.swift
@@ -6,29 +6,32 @@ enum SecCertificateError<S, F> {
 }
 
 extension SecCertificate {
-    
     enum Certificate {
         static let cert = ""
     }
-    
+
     static var hashKey: String {
         ""
     }
-    
-    static func loadFromBundle(certName: String = Certificate.cert,
-                               bundle: Bundle = Bundle.main) -> SecCertificateError<SecCertificate, String> {
-        
-        guard let filePath = bundle.path(forResource: certName, ofType: "cer") else {
+
+    static func loadFromBundle(
+        certName: String = Certificate.cert,
+        bundle: Bundle = Bundle.main
+    ) -> SecCertificateError<SecCertificate, String> {
+        guard let filePath = bundle.path(
+            forResource: certName,
+            ofType: "cer"
+        ) else {
             return .failure("Couldn't load resource from \(bundle)")
         }
-        
+
         do {
             let data = try Data(contentsOf: URL(fileURLWithPath: filePath))
             guard let certificate = SecCertificateCreateWithData(nil, data as CFData) else {
                 return .failure("Couldn't convert data as SecCertificateCreateWithData for file \(filePath)")
             }
             return .success(certificate)
-            
+
         } catch {
             return .failure(error.localizedDescription)
         }
